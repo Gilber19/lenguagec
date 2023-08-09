1fb60df42217293aa1ba233d8646f5ff8fdd23c8
Gilber19 <gilberto.diarte@uabc.edu.mx>
2023-08-09 16:10:09 -0700

    new file:   Practica1_ejercicio1_Diarte_Gilberto.c

---
+++ Practica1_ejercicio1_Diarte_Gilberto.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (){
+
+int limite;
+int resultado;
+
+printf("Ingrese un numero entero positivo: ");
+scanf("%d", & limite);
+for (int i=0;i<=limite;i++){
+    resultado +=i;
+}
+printf("la suma de los primeros %d numeros es: %d\n", limite, resultado);
+
+
+return 0;
+
+}
